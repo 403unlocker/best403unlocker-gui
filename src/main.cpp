7b3a9949b163326b8c7b8d3691df6067b91fb0b0
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2024-10-23 22:06:01 +0330

    feat: load icon asset for app and use .h for header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
-#include "main.hpp"
+#include "main.h"
 
 #include <wx/app.h>
+#include <wx/image.h>
 
-#include "main_frame.hpp"
+#include "main_frame.h"
 
 wxIMPLEMENT_APP(Unlocker);
 
 bool Unlocker::OnInit() {
+  wxImage::AddHandler(new wxXPMHandler{});
+  wxIcon icon{"./assets/icon.xpm", wxBITMAP_TYPE_XPM};
+
   MainFrame *mainFrame = new MainFrame();
   mainFrame->SetWindowStyle(wxDEFAULT_FRAME_STYLE ^ wxMAXIMIZE_BOX ^
                             wxRESIZE_BORDER);
   mainFrame->SetClientSize(300, 500);
+  mainFrame->SetIcon(icon);
   mainFrame->Center();
   mainFrame->Show();
   return true;
---
+++ src/main.h
@@ -0,0 +1,11 @@
+#ifndef BEST403UNLOCKER_MAIN_H_
+#define BEST403UNLOCKER_MAIN_H_
+
+#include <wx/app.h>
+
+class Unlocker : public wxApp {
+ private:
+  bool OnInit();
+};
+
+#endif
