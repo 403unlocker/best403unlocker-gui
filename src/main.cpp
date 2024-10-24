d5ac0234f51ba463fb865ed5719a3970af5e32f7
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2024-10-24 18:23:42 +0330

    refactor: embed icon into executable

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,19 @@
 #include <wx/app.h>
 #include <wx/image.h>
 
+#include "../assets/icon.xpm"
 #include "main_frame.h"
 
 wxIMPLEMENT_APP(Unlocker);
 
 bool Unlocker::OnInit() {
   wxImage::AddHandler(new wxXPMHandler{});
-  wxIcon icon{"./assets/icon.xpm", wxBITMAP_TYPE_XPM};
 
   MainFrame *mainFrame = new MainFrame();
   mainFrame->SetWindowStyle(wxDEFAULT_FRAME_STYLE ^ wxMAXIMIZE_BOX ^
                             wxRESIZE_BORDER);
   mainFrame->SetClientSize(300, 500);
-  mainFrame->SetIcon(icon);
+  mainFrame->SetIcon(iconImg);
   mainFrame->Center();
   mainFrame->Show();
   return true;
